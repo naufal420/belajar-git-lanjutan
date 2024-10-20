6efae4cf70f29121659c42d24216f559007eb8f1
Naufal Ikbar <naufalikbar201@gmail.com>
2024-10-20 22:53:32 +0700

    menambah fungsi add dan subtract

--- fungsi.js
+++ fungsi.js
@@ -1,3 +1,5 @@
-console.log("fungsi pertama");
-console.log("fungsi pertama");
-console.log("fungsi pertama");
+const add = (a, b) => a + b;
+console.log((1,2));
+
+const subtract = (a,b) => a - b;
+console.log(subtract(20,10));
