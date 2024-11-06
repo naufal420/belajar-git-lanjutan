6b64734c4ca12f202c94317dd06a0f6ac20d6d03
Naufal Ikbar <naufalikbar201@gmail.com>
2024-11-06 13:29:50 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log((1,2));
 const subtract = (a,b) => a - b;
 console.log(subtract(20,10));
 
-const multiply = (a,b) => a * b;
-console.log(subtract(20,5));
+const multiplyNumber = (a,b) => a * b;
+console.log(multiplyNumber(20,5));
 
 const divide = (a,b) => a / b;
 console.log(subtract(25,5));
